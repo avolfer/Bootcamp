ec4041c827f551a53ca7c0a4760e49909942c5d9
avolfer <jakubstuba2004@gmail.com>
2023-03-21 18:20:05 +0100

    Update

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-# data types:
+# stan_konta = input("Ile masz kasy?")
+# stan_konta = int(stan_konta)
+# stan_konta = stan_konta + 500 * 2
+# print(stan_konta)
 
 
+# x = 9
+# y = x/2
+#
+# print(y)
+
+temperature = -5
+happy = False
+
+if temperature > 10 or happy:
+    print("git idę")
+elif temperature > -10:
+    print("ciepło ubierz się")
+else:
+    print("nie git")
